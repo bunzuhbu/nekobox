705ca8b2d1df838fb8533ec3487000d79ca26cc8
qr243vbi <uiguruigurovich@gmail.com>
2025-08-25 06:57:35 +0300

    Update deploy_vendor.sh

--- script/deploy_vendor.sh
+++ script/deploy_vendor.sh
@@ -1,12 +1,12 @@
 source script/env_deploy.sh
 
 pushd "$SRC_ROOT"/core/server
-go mod vendor
-mkdir -p "$DEPLOYMENT/gen"
-mv -T vendor "$DEPLOYMENT/vendor"
 pushd gen
   protoc -I . --go_out="$DEPLOYMENT/gen" --protorpc_out="$DEPLOYMENT/gen" libcore.proto
 popd
+go mod vendor
+mkdir -p "$DEPLOYMENT/gen"
+mv -T vendor "$DEPLOYMENT/vendor"
 curl https://api.github.com/repos/sagernet/sing-box/releases/latest | jq -r '.name' > "$DEPLOYMENT/Sagernet.SingBox.Version.txt"
 popd
 
