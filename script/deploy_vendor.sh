50073feb2312f5406b99368fa40e711cfd68d696
u <u>
2025-08-25 09:29:22 +0300

    update

--- script/deploy_vendor.sh
+++ script/deploy_vendor.sh
@@ -2,11 +2,10 @@ source script/env_deploy.sh
 
 pushd "$SRC_ROOT"/core/server
 mkdir -p "$DEPLOYMENT/gen"
-pushd gen
+pushd "$DEPLOYMENT/gen"
   protoc -I . --go_out="$DEPLOYMENT/gen" --protorpc_out="$DEPLOYMENT/gen" libcore.proto
 popd
-go get github.com/chai2010/protorpc
-go get github.com/golang/protobuf/proto
+
 go mod tidy
 go mod vendor
 mv -T vendor "$DEPLOYMENT/vendor"
