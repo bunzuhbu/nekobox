b55b4b165dabd687af1d2c10778ccc8d8719fd84
qr243vbi <uiguruigurovich@gmail.com>
2025-09-07 13:21:01 +0300

    update

--- script/deploy_windows.sh
+++ script/deploy_windows.sh
@@ -26,7 +26,7 @@ curl -fLJO https://github.com/rainers/cv2pdb/releases/download/v0.53/cv2pdb-0.53
 ./cv2pdb/cv2pdb64.exe ./build/nekobox.exe ./tmp.exe ./nekobox.pdb
 rm -rf cv2pdb-0.53.zip cv2pdb
 cd build
-strip -s nekobox.exe
+strip -s nekobox.exe ||:
 cd ..
 rm tmp.exe
 mv nekobox.pdb $DEST
