00f4b4f94ffbed789170367fffef391a3235484b
qr243vbi <uiguruigurovich@gmail.com>
2025-09-11 15:38:54 +0300

    Update deploy_linux64.sh

--- script/deploy_linux64.sh
+++ script/deploy_linux64.sh
@@ -18,7 +18,7 @@ rm -rf $DEST
 mkdir -p $DEST
 
 #### copy srslist ####
-cp download-artifact/*public*public*/srslist $DEST/srslist
+cp download-artifact/*public*/srslist $DEST/srslist
 
 #### copy binary ####
 cp $BUILD/nekobox $DEST
