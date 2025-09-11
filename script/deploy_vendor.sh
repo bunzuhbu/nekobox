a47b90ed8c4ef052a3204662f0af8a43d10bfd35
qr243vbi <uiguruigurovich@gmail.com>
2025-09-11 16:39:11 +0300

    Update deploy_vendor.sh

--- script/deploy_vendor.sh
+++ script/deploy_vendor.sh
@@ -7,10 +7,6 @@ pushd gen
   protoc -I . --go_out="$DEPLOYMENT/gen" --protorpc_out="$DEPLOYMENT/gen" libcore.proto
 popd
 
-curl -fLso "$DEPLOYMENT/srslist.h" "https://raw.githubusercontent.com/throneproj/routeprofiles/rule-set/srslist.h"
-curl -fLso "$DEPLOYMENT/srslist" "https://raw.githubusercontent.com/throneproj/routeprofiles/rule-set/list"
-
-
 go mod tidy
 go mod vendor
 mv -T vendor "$DEPLOYMENT/vendor"
